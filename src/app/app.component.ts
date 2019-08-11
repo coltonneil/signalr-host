af12691a6c6b91eb91b181a7e062ad3735aedaca
Colton Williams <coltonneilwilliams@gmail.com>
2019-08-10 21:21:06 -0700

    Add private messaging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import * as signalR from '@aspnet/signalr';
+import { Component, OnInit } from "@angular/core";
+import * as signalR from "@aspnet/signalr";
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: `app-root`,
+  templateUrl: `./app.component.html`,
+  styleUrls: [`./app.component.scss`]
 })
 export class AppComponent implements OnInit {
-  title = 'signalr-client';
+  title = `signalr-client`;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     const connection = new signalR.HubConnectionBuilder()
-      .configureLogging(signalR.LogLevel.Trace)
-      .withUrl(`http://localhost:5000/notify`)
+      .configureLogging(signalR.LogLevel.None)
+      .withUrl(`http://localhost:5000/messages`)
       .build();
 
-    connection.start().then(() => {
-      console.log('Connected!');
-    }).catch(err => {
-      return console.error(err.toString());
+    connection
+      .start()
+      .then(() => {
+        console.log(`Connected!`);
+      })
+      .catch(err => {
+        return console.error(err.toString());
+      });
+
+    connection.on(`ReceiveMessage`, (payload: string) => {
+      console.log(`message recieved: ${payload}`);
     });
 
-    connection.on(`BroadcastMessage`, (type: string, payload: string) => {
-      console.log(`message recieved: ` + payload);
+    connection.on(`UserConnected`, (connectionId: string) => {
+      console.log(`connected: ${connectionId}`);
+      connection.invoke(`JoinGroup`, `PrivateGroup`).then(() => {
+        console.log(`joining group: PrivateGroup`);
+      });
     });
+
   }
 }
